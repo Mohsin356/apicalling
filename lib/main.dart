4e7c00583bd4d464601e60ea51b99ed7d20fa692
Mohsin <Mohsin356>
2022-04-18 05:09:34 +0500

    single user get

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,7 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
-Future<Album> fetchAlbum() async {
-  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return Album.fromJson(jsonDecode(response.body));
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
-  }
-}
-class Album {
-  final int userId;
-  final int id;
-  final String title;
-
-  const Album({
-    required this.userId,
-    required this.id,
-    required this.title,
-  });
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      userId: json['userId'],
-      id: json['id'],
-      title: json['title'],
-    );
-
-  }
-}
+import 'package:apicalling/models/album.dart';
 
 void main() => runApp(const MyApp());
 
@@ -46,12 +13,21 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // late Future<Album> futureAlbum;
-
+  var peoples;
+  void fetchAlbum() async {
+    try{
+      final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+      Map<String, dynamic> serverData = jsonDecode(response.body);
+      final jsonData= Album.fromJson(serverData);
+      setState(() {
+        peoples=jsonData;
+      });
+    }catch(err) {}
+  }
   @override
   void initState() {
     super.initState();
-    // futureAlbum = fetchAlbum();
+    fetchAlbum();
   }
 
   @override
@@ -65,21 +41,11 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Fetch Data Example'),
         ),
-        body: Center(
-          child: FutureBuilder<Album>(
-            future: fetchAlbum(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data!.title);
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
+        body:
 
-              // By default, show a loading spinner.
-              return const CircularProgressIndicator();
-            },
-          ),
-        ),
+               Text('title:\n ${peoples.title}\n'),
+
+
       ),
     );
   }
---
+++ lib/models/album.dart
@@ -0,0 +1,21 @@
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  const Album({
+    required this.userId,
+    required this.id,
+    required this.title,
+  });
+
+  Album.fromJson(Map<String, dynamic> json)
+      : userId= json['userId'],
+        id= json['id'],
+        title=json['title'];
+  Map<String,dynamic>toJason()=>{
+    'userId':userId,
+    'id':id,
+    'title':title,
+  };
+}
